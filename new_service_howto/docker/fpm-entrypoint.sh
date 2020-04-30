e4f80836fb47f0af310e95f590069dbb816eb2a6
jygaulier <gaulier@alchemy.fr>
2020-04-30 19:06:24 +0200

    WIP

---
+++ new_service_howto/docker/fpm-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ${XDEBUG_ENABLED} == "1" ]; then
+    docker-php-ext-enable xdebug
+fi
+
+# Must refresh cache to interpret /configs/config.json
+if [ "${APP_ENV}" == "prod" ]; then
+    su app /bin/sh -c "bin/console cache:clear"
+fi
+
+exec "$@"
